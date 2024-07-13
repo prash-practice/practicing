1e462b40b7affc58441ceb1d56030c05d705ea56
PrashanthAgithe <prashanthagithe@gmail.com>
2024-07-13 13:55:19 +0530

    bugFixed

--- practice.js
+++ practice.js
@@ -1,17 +1,3 @@
-// var regexp=/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/;
-// var reg_ph=/^(?=.*[A-Z])(?=.*[\d])(?=.*[@#_])[A-Za-z\d@#_]{4,8}$/;
-// function validate(){
-//   let email=document.getElementById('email').value;
-//   let pass=document.getElementById('password').value;
-//   let message=document.getElementById('msg');
-//   if(!reg_ph.test(pass)){
-//     message.innerHTML="invalid pass";
-//     message.style.color="red";
-//   }else{
-//     message.innerHTML="valid pass"
-//     message.style.color="green";
-//   }
-// }
 let checkboxes=document.querySelectorAll('.checkbox');
 checkboxes.forEach(checkbox=>{
   checkbox.addEventListener('change',()=>{
