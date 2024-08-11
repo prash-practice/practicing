de50be020d6e1e22548c966e86fc6a8ba9bb909d
PrashanthAgithe <prashanthagithe@gmail.com>
2024-08-11 18:38:29 +0530

    bug fixed

--- practice.js
+++ practice.js
@@ -1,10 +1,1 @@
-let checkboxes=document.querySelectorAll('.checkbox');
-checkboxes.forEach(checkbox=>{
-  checkbox.addEventListener('change',()=>{
-    let checkedcheckboxes=document.querySelectorAll('.checkbox:checked')
-    if(checkedcheckboxes.length >2){
-      alert("can only select 2 options")
-      checkbox.checked = false; 
-    }
-  })
-})
+document.getElementById('heading').style.color='green'
